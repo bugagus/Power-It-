41d45581a7c2a2376e8d5ed9ee9ec2734d3885f3
Agustin Triguero Noriega <101557679+bugagus@users.noreply.github.com>
2024-04-15 13:04:35 +0200

    Input Controller y Movimiento basico

---
+++ Power It!/Assets/Scripts/InputManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class InputManager : MonoBehaviour
+{
+     public InputControls _input;
+    private JumpController _jumpController;
+
+    private void Awake()
+    {
+        _input = new InputControls();
+        _jumpController = gameObject.GetComponent<JumpController>();
+    }
+
+    private void OnEnable()
+    {
+        _input.Enable();
+    }
+
+    private void OnDestroy()
+    {
+        _input.Disable();
+    }
+
+    private void Start()
+    {
+        _input.Controls.LeftHand.started += ctx => StartLeftHand(ctx);   
+        _input.Controls.LeftHand.canceled += ctx => EndLeftHand(ctx);
+        _input.Controls.RightHand.started += ctx => StartRightHand(ctx);
+        _input.Controls.RightHand.canceled += ctx => EndRightHand(ctx);
+    }
+
+    private void StartRightHand(InputAction.CallbackContext context)
+    {
+        _jumpController.GrabRight();
+        Debug.Log("Pulso la E");
+    }
+
+    private void EndRightHand(InputAction.CallbackContext context)
+    {
+        _jumpController.ReleaseRight();
+    }
+
+    private void StartLeftHand(InputAction.CallbackContext context)
+    {
+        _jumpController.GrabLeft();
+        Debug.Log("Pulso la Q");
+    }
+
+    private void EndLeftHand(InputAction.CallbackContext context)
+    {
+        _jumpController.ReleaseLeft();
+    }
+}
